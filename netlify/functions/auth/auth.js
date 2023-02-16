74d32ce6fac1b865d52e31fff690396c04e9ed17
Andrey Lushnikov <lushnikov@chromium.org>
2023-02-16 15:35:29 +0600

    chore: fix function

--- netlify/functions/auth/auth.js
+++ netlify/functions/auth/auth.js
@@ -1,16 +1,21 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
+  if (!event.body)
+    return { statusCode: 400, body: 'missing body' };
+  let body;
   try {
-    const { username } = JSON.parse(event.body);
-    return {
-      statusCode: 200,
-      headers: {
-        'Set-Cookie': `username=${username};`,
-      },
-    };
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    body = JSON.parse(event.body);
+  } catch (e) {
+    return { statusCode: 400, body: 'failed to parse body as JSON' };
   }
+  if (!body.username)
+    return { statusCode: 400, body: 'JSON body must have a single "username" entry with a name' };
+  return {
+    statusCode: 200,
+    headers: {
+      'Set-Cookie': `username=${username};`,
+    },
+  };
 }
 
 module.exports = { handler }
